5ecc068ab4622e2eb88ac866e9947970e327cdb3
gosseau1993 <jordanafpa@gmail.com>
2022-12-29 16:44:19 +0100

    cv interactif v0.3

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
   /* Chercher les liens*/
 
+  /*Chercher Experience*/
   let blocExperience = document.querySelector(".experience");
   console.log(blocExperience);
 
   let fermerExperience = document.querySelector(".fermerExperience");
   console.log(fermerExperience);
 
+  /*Chercher Competences*/
   let blocCompetences = document.querySelector(".competences");
   console.log(blocCompetences);
 
   let fermerCompetences = document.querySelector(".fermerCompetences");
   console.log(fermerCompetences);
 
+  /*Chercher Formation*/
   let blocFormation = document.querySelector(".formation");
   console.log(blocFormation);
 
   let fermerFormation = document.querySelector(".fermerFormation");
   console.log(fermerFormation);
 
+  /*Chercher A propos de moi*/
+  let blocPersonnel = document.querySelector(".personnel");
+  console.log(blocPersonnel);
+
+  let fermerPersonnel = document.querySelector(".fermerPersonnel");
+  console.log(fermerPersonnel);
+
+  /* Chercher le blocPage*/
   let blocPage = document.querySelector(".bloc_page");
   console.log(blocPage);
 
@@ -54,5 +65,15 @@ document.addEventListener("DOMContentLoaded", function () {
   fermerFormation.addEventListener("click", function (e) {
     e.preventDefault();
     blocPage.classList.remove("menuOpenFormation");
+  });
+
+  /*Personnel Appel*/
+  blocPersonnel.addEventListener("click", function (e) {
+    e.preventDefault();
+    blocPage.classList.add("menuOpenPersonnel");
+  });
+  fermerPersonnel.addEventListener("click", function (e) {
+    e.preventDefault();
+    blocPage.classList.remove("menuOpenPersonnel");
   });
 });
